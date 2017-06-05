03496740e70052e5a317b06d1581987a93592080
linev_a <linev_a@master.nngu>
2017-06-05 23:35:10 +0300

    run data

--- testOpenMp/copyEulerOpenMP.sh
+++ testOpenMp/copyEulerOpenMP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-PATH_PROGRAM=$(find ../build -print0 | grep -FzZ 'Release/app_modules_OpenMP_Euler.exe')
+PATH_PROGRAM=$(find ../build -print0 | grep -FzZ 'app/app_modules_OpenMP_Euler')
 echo "$PATH_PROGRAM"
 cp -f "$PATH_PROGRAM" .
--- testOpenMp/copyRungeOpenMP.sh
+++ testOpenMp/copyRungeOpenMP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-PATH_PROGRAM=$(find ../build -print0 | grep -FzZ 'Release/app_modules_OpenMP_Runge.exe')
+PATH_PROGRAM=$(find ../build -print0 | grep -FzZ 'app/app_modules_OpenMP_Runge')
 echo "$PATH_PROGRAM"
 cp -f "$PATH_PROGRAM" .
