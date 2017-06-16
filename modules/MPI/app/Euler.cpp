d2339eaf174f9529d66eb4d673778a2eece22e57
Unknown <DeysonSH@gmail.com>
2017-06-16 20:25:33 +0300

    MPI Euler try to fix

--- modules/MPI/app/Euler.cpp
+++ modules/MPI/app/Euler.cpp
@@ -43,17 +43,6 @@ int main(int argc, char **argv) {
         outfilename = argv[3];
         threads = atoi(argv[4]);
 
-
-        //string outfilename = "../../../../../result/Sergey-N/MPI_Euler_1.txt";
-        // File variables
-//        string functionFile = "../../initial/function.txt";
-        //string functionFile = "../../../../../initial/function2.txt";
-//        string functionFile = "../../../../../initial_test/function.txt";
-//        string settingFile = "../../initial/setting.ini";
-//        string settingFile = "../../../../../initial/setting.ini";
-        //string settingFile = "../../../../../initial/setting2.ini";
-//        string settingFile = "../../../../../initial_test/setting.ini";
-
         // Read task settings
         initTaskUsingFile(task, settingFile);
         setTimestep(task);
@@ -94,26 +83,27 @@ int main(int argc, char **argv) {
     }
     int full_sizeX = task.nX + 2;    // define addition boundaries as default condition
     int sizeY = full_sizeX;          // size of one "y" cell
-    int sizeZ = sizeY * task.nY;
+    int sizeZ = sizeY * task.nY;    // size of full plast (layer) of values
 
-    int lineSizeP = 2;                      // proc in line
+    int lineP = 2;                      // proc in line
     int rowP = sizeP / 2;
     if (sizeP % 2 != 0) {
         printf("%d don't divided by 2", sizeP);
         exit(0);
     }
     int proc_nX = full_sizeX;               // already added as default boundaries cell
-    int proc_nY = task.nY / lineSizeP + 2;  // there and after +2 as boundaries conditions
-    if (task.nY % lineSizeP != 0) {
+    int proc_nY = task.nY / lineP + 2;  // there and after +2 as boundaries conditions
+    if (task.nY % lineP != 0) {
         printf("nY %d don't divided by %d", task.nY, sizeP);
         exit(0);
     }
     int proc_nZ = task.nZ / rowP + 2;
-    if (task.nY % lineSizeP != 0) {
-        printf("nZ %d don't divided by rowP %d", task.nY, rowP);
+    if (task.nZ % rowP != 0) {
+        printf("nZ %d don't divided by rowP %d", task.nZ, rowP);
         exit(0);
     }
     int proc_vect_size = proc_nX * proc_nY * proc_nZ;
+
 
 
 
@@ -121,7 +111,7 @@ int main(int argc, char **argv) {
     int proc_sizeY = full_proc_sizeX;
     int proc_sizeZ = proc_sizeY * proc_nY;  // this is size of one line of data
 
-    int block_size_without_boundaries = proc_nX * (task.nY / lineSizeP);
+    int block_size_without_boundaries = proc_nX * (task.nY / lineP);
     int block_size = proc_sizeZ;
 
     // Generate data for send
@@ -131,15 +121,15 @@ int main(int argc, char **argv) {
         sendvect = new double[sizeP * proc_vect_size];
 
         for (int i = 0; i < sizeP * proc_vect_size; ++i) {
-            sendvect[i] = 0;
+            sendvect[i] = -9000000000;
         }
 //
-//        cout << "sizeP: " << sizeP << endl;
-//        cout << "proc_nX: " << proc_nX << endl;
-//        cout << "proc_nY: " << proc_nY << endl;
-//        cout << "proc_nZ: " << proc_nZ << endl;
-//        cout << "block_size: " << block_size << endl;
-//        cout << "proc_vect_size: " << proc_vect_size << endl;
+        cout << "sizeP: " << sizeP << endl;
+        cout << "proc_nX: " << proc_nX << endl;
+        cout << "proc_nY: " << proc_nY << endl;
+        cout << "proc_nZ: " << proc_nZ << endl;
+        cout << "block_size: " << block_size << endl;
+        cout << "proc_vect_size: " << proc_vect_size << endl;
 
 //        printf("SEND!!!!!!\n");
 //        for (int i = 0; i < task.fullVectSize; ++i) {
@@ -150,8 +140,8 @@ int main(int argc, char **argv) {
         int position = 0;
         for (int p = 0; p < sizeP; ++p) {
 
-            int y_offset = (p % lineSizeP) * block_size_without_boundaries;
-            int z_offset = (p / lineSizeP) * lineSizeP * (proc_nZ - 2) * block_size_without_boundaries;
+            int y_offset = (p % lineP) * block_size_without_boundaries;
+            int z_offset = (p / lineP) * lineP * (proc_nZ - 2) * block_size_without_boundaries;
 
             for (int z = 0; z < proc_nZ; ++z) {
                 for (int i = 0, j = 0; i < block_size; ++i) {
@@ -168,6 +158,8 @@ int main(int argc, char **argv) {
             }
         }
 
+        std::cout << "Flag 2" << std::endl;
+        fflush(stdout);
 //        printf("=========\n");
 //        for (int i = 0; i < sizeP * proc_vect_size; ++i) {
 //            printf( "%2.15le\n", sendvect[i]);
@@ -190,6 +182,7 @@ int main(int argc, char **argv) {
 
     MPI_Scatterv(sendvect, sendcounts, displs, MPI_DOUBLE,
                  proc_vect[0], proc_vect_size, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+
 
 
 
@@ -226,68 +219,88 @@ int main(int argc, char **argv) {
 
         // Before each iteration - swap data
         // 1 and 2 iter - IT'S TIME TO REPACK
+//
+//        for (int z = 0; z < proc_nZ; ++z) {
+//            for (int i = 0; i < proc_nX; ++i) {
+//                //z * proc_sizeZ + i
+//                left_vect[z * proc_nX + i] = proc_vect[currTime][z * proc_sizeZ + proc_sizeY + i];
+//                //z * proc_sizeZ + (proc_nY - 1) * proc_sizeY + i
+//                right_vect[z * proc_nX + i] = proc_vect[currTime][z * proc_sizeZ + (proc_nY - 2) * proc_sizeY + i];
+//            }
+//        }
+//
+//        int left_proc = (rankP / lineSizeP) * lineSizeP + (rankP - 1 + lineSizeP) % lineSizeP;
+//        int right_proc = (rankP / lineSizeP) * lineSizeP + (rankP + 1 + lineSizeP) % lineSizeP;
+//
+//        MPI_Sendrecv(left_vect, send_vect_size, MPI_DOUBLE, left_proc, 1,
+//            left_vect_get, send_vect_size, MPI_DOUBLE, right_proc, 1, MPI_COMM_WORLD, &status);
+////
+//        MPI_Sendrecv(right_vect, send_vect_size, MPI_DOUBLE, right_proc, 1,
+//            right_vect_get, send_vect_size, MPI_DOUBLE, left_proc, 1, MPI_COMM_WORLD, &status);
+////
+//        for (int z = 0; z < proc_nZ; ++z) {
+//            for (int i = 0; i < proc_nX; ++i) {
+//                proc_vect[currTime][z * proc_sizeZ + i] = left_vect_get[z * proc_nX + i];
+//                proc_vect[currTime][z * proc_sizeZ + (proc_nY - 1) * proc_sizeY + i] =
+//                    right_vect_get[z * proc_nX + i];
+//            }
+//        }
+////
+//        int top_proc = (rankP - lineSizeP + sizeP) % sizeP;
+//        int bottom_proc = (rankP + lineSizeP) % sizeP;
+//
+////        cout << "top_proc: " << top_proc << endl;
+////        cout << "bottom_proc: " << bottom_proc << endl;
+//
+//        // 1 and 2 iter - in one field in the memory
+//        send_vect_size = proc_nY * proc_nX;
+//        // top
+//        MPI_Sendrecv(proc_vect[currTime] + proc_sizeZ, send_vect_size, MPI_DOUBLE, top_proc, 2,
+//                     proc_vect[currTime], send_vect_size, MPI_DOUBLE, bottom_proc, 2, MPI_COMM_WORLD, &status);
+//
+//        // bottom
+//        int offset = proc_vect_size - send_vect_size;
+//        MPI_Sendrecv(proc_vect[currTime] + offset - proc_sizeZ, send_vect_size, MPI_DOUBLE, bottom_proc, 2,
+//                     proc_vect[currTime] + offset, send_vect_size, MPI_DOUBLE, top_proc, 2, MPI_COMM_WORLD, &status);
+//
+////
+//        int boundaries_offset = proc_sizeY;
+//
+//        // BOUNDARIES!
+//        for (int k = 0; k < proc_nY * proc_nZ; ++k) {
+//            proc_vect[currTime][k * boundaries_offset] = proc_vect[currTime][k * boundaries_offset + 1];
+//            proc_vect[currTime][k * boundaries_offset + proc_nX - 1] =
+//                    proc_vect[currTime][k * boundaries_offset + proc_nX - 2];
+//        }
 
-        for (int z = 0; z < proc_nZ; ++z) {
-            for (int i = 0; i < proc_nX; ++i) {
-                //z * proc_sizeZ + i
-                left_vect[z * proc_nX + i] = proc_vect[currTime][z * proc_sizeZ + proc_sizeY + i];
-                //z * proc_sizeZ + (proc_nY - 1) * proc_sizeY + i
-                right_vect[z * proc_nX + i] = proc_vect[currTime][z * proc_sizeZ + (proc_nY - 2) * proc_sizeY + i];
+//        if (rankP != 2) {
+////            multiplicateVector(spMat, proc_vect[currTime], proc_vect[prevTime], proc_vect_size);
+//            ;
+//        } else {
+//            for (int i = 0; i < proc_vect_size; ++i) {
+//                proc_vect[prevTime][i] == -999999999;
+//            }
+//        }
+        if (rankP == 5) {
+            for (int i = 0; i < proc_vect_size; ++i) {
+                proc_vect[prevTime][i] = -999999999;
+                proc_vect[currTime][i] = -999999999;
             }
         }
-
-        int left_proc = (rankP / lineSizeP) * lineSizeP + (rankP - 1 + lineSizeP) % lineSizeP;
-        int right_proc = (rankP / lineSizeP) * lineSizeP + (rankP + 1 + lineSizeP) % lineSizeP;
-
-        MPI_Sendrecv(left_vect, send_vect_size, MPI_DOUBLE, left_proc, 1,
-            left_vect_get, send_vect_size, MPI_DOUBLE, right_proc, 1, MPI_COMM_WORLD, &status);
-//
-        MPI_Sendrecv(right_vect, send_vect_size, MPI_DOUBLE, right_proc, 1,
-            right_vect_get, send_vect_size, MPI_DOUBLE, left_proc, 1, MPI_COMM_WORLD, &status);
-//
-        for (int z = 0; z < proc_nZ; ++z) {
-            for (int i = 0; i < proc_nX; ++i) {
-                proc_vect[currTime][z * proc_sizeZ + i] = left_vect_get[z * proc_nX + i];
-                proc_vect[currTime][z * proc_sizeZ + (proc_nY - 1) * proc_sizeY + i] =
-                    right_vect_get[z * proc_nX + i];
-            }
-        }
-//
-        int top_proc = (rankP - lineSizeP + sizeP) % sizeP;
-        int bottom_proc = (rankP + lineSizeP) % sizeP;
-
-//        cout << "top_proc: " << top_proc << endl;
-//        cout << "bottom_proc: " << bottom_proc << endl;
-
-        // 1 and 2 iter - in one field in the memory
-        send_vect_size = proc_nY * proc_nX;
-        // top
-        MPI_Sendrecv(proc_vect[currTime] + proc_sizeZ, send_vect_size, MPI_DOUBLE, top_proc, 2,
-                     proc_vect[currTime], send_vect_size, MPI_DOUBLE, bottom_proc, 2, MPI_COMM_WORLD, &status);
-
-        // bottom
-        int offset = proc_vect_size - send_vect_size;
-        MPI_Sendrecv(proc_vect[currTime] + offset - proc_sizeZ, send_vect_size, MPI_DOUBLE, bottom_proc, 2,
-                     proc_vect[currTime] + offset, send_vect_size, MPI_DOUBLE, top_proc, 2, MPI_COMM_WORLD, &status);
-
-//
-        int boundaries_offset = proc_sizeY;
-
-        // BOUNDARIES!
-        for (int k = 0; k < proc_nY * proc_nZ; ++k) {
-            proc_vect[currTime][k * boundaries_offset] = proc_vect[currTime][k * boundaries_offset + 1];
-            proc_vect[currTime][k * boundaries_offset + proc_nX - 1] =
-                    proc_vect[currTime][k * boundaries_offset + proc_nX - 2];
-        }
-
-        multiplicateVector(spMat, proc_vect[currTime], proc_vect[prevTime], proc_vect_size);
-
 
         prevTime = (prevTime + 1) % 2;
         currTime = (currTime + 1) % 2;
     }
+    std::cout << "Flag" << std::endl;
+    fflush(stdout);
 
     // And now we should scatter it in one vector...
+
+    sendvect = new double[sizeP * proc_vect_size];
+
+    for (int i = 0; i < sizeP * proc_vect_size; ++i) {
+        sendvect[i] = -9000000000;
+    }
 
     MPI_Gather(proc_vect[prevTime], proc_vect_size, MPI_DOUBLE,
                sendvect, proc_vect_size, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
@@ -325,15 +338,15 @@ int main(int argc, char **argv) {
         cout << "block_size: " << block_size << endl;
         cout << "proc_vect_size: " << proc_vect_size << endl;
         cout << "sizeZ: " << sizeZ << endl;
-        cout << "lineSizeP: " << lineSizeP << endl;
+        cout << "lineSizeP: " << lineP << endl;
 
 
         int temp;
         int position = 0;
         for (int p = 0; p < sizeP; ++p) {
 
-            int y_offset = (p % lineSizeP) * block_size_without_boundaries;
-            int z_offset = (p / lineSizeP) * lineSizeP * (proc_nZ - 2) * block_size_without_boundaries;
+            int y_offset = (p % lineP) * block_size_without_boundaries;
+            int z_offset = (p / lineP) * lineP * (proc_nZ - 2) * block_size_without_boundaries;
 
             for (int z = 0; z < proc_nZ; ++z) {
                 for (int i = 0, j = 0; i < block_size; ++i) {
